51d0bf97aacf54c2f3df8947bca4763532299644
Your Name <you@example.com>
2025-10-16 01:52:59 +0600

    testing

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World\n";
+    int num1;
+    int num2;
+
+    cout << "What is your first number?\n";
+    cin >> num1;
+
+    cout << "What is your second number?\n";
+    cin >> num2;
+
+    cout << "Sum = " << num1 + num2 << endl;
+    cout << "Difference = " << num1 - num2 << endl;
+    cout << "Product = " << num1 * num2 << endl;
+    cout << "Quotient = " << num1 / num2 << endl;
     return 0;
 }
